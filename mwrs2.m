e1bada9d0a9b909f2e9d2ca7f82cd438742be1a3
560059 <design.nss.rvce@gmail.com>
2025-03-14 07:49:11 +0530

    Update mwrs2.m

--- mwrs2.m
+++ mwrs2.m
@@ -1,64 +1,67 @@
-10th
-% Parameters
-nFFT = 64;          % FFT size
-nDSC = 52;          % Number of data subcarriers
-nSym = 1e4;         % Number of OFDM symbols
-EbN0dB = 0:10;      % Eb/N0 range in dB
-EsN0dB = EbN0dB + 10*log10(nDSC/nFFT) + 10*log10(64/80); % Es/N0
-
-% Preallocate
-nErr = zeros(1, length(EbN0dB));
-
-for ii = 1:length(EbN0dB)
-    % Transmitter
-    ipBit = rand(1, nDSC * nSym) > 0.5; % Random bits
-    ipMod = 2 * ipBit - 1;              % BPSK modulation
-    ipMod = reshape(ipMod, nDSC, nSym).'; % Reshape to symbols
+clear all 
+nFFT = 64; 
+nDSC = 52;  
+nBitPerSym = 52;  
+nSym = 10^4; 
+EbN0dB = [0:10];
+EsN0dB = EbN0dB + 10*log10(nDSC/nFFT) + 10*log10(64/80); % converting to symbol to noise ratio 
+for ii = 1:length(EbN0dB) 
+% Transmitter 
+ipBit = rand(1,nBitPerSym*nSym) > 0.5; % random 1's and 0's 
+ ipMod = 2*ipBit-1; % BPSK modulation 0 --> -1, 1 --> +1 
+   ipMod = reshape(ipMod,nBitPerSym,nSym).'; % grouping into multiple symbolsa 
+  
+   % Assigning modulated symbols to subcarriers from [-26 to -1, +1 to +26] 
+   xF = [zeros(nSym,6) ipMod(:,[1:nBitPerSym/2]) zeros(nSym,1) 
+ipMod(:,[nBitPerSym/2+1:nBitPerSym]) zeros(nSym,5)] ; 
     
-    % Map to subcarriers
-    xF = [zeros(nSym, 6), ipMod(:, 1:nDSC/2), zeros(nSym, 1), ipMod(:, nDSC/2+1:nDSC), zeros(nSym, 5)];
-    
-    % IFFT and normalize power
-    xt = (nFFT/sqrt(nDSC)) * ifft(fftshift(xF.')).';
-    
-    % Add cyclic prefix
-    xt = [xt(:, 49:64), xt];
-    xt = reshape(xt.', 1, nSym * 80);
-    
-    % Noise
-    nt = (1/sqrt(2)) * (randn(1, nSym * 80) + 1j * randn(1, nSym * 80));
-    yt = sqrt(80/64) * xt + 10^(-EsN0dB(ii)/20) * nt;
-    
-    % Receiver
-    yt = reshape(yt.', 80, nSym).';
-    yt = yt(:, 17:80); % Remove cyclic prefix
-    
-    % FFT
-    yF = (sqrt(nDSC)/nFFT) * fftshift(fft(yt.')).';
-    yMod = yF(:, [6+(1:nDSC/2), 7+(nDSC/2+1:nDSC)]);
-    
-    % Demodulate
-    ipModHat = sign(real(yMod)); % BPSK demodulation
-    ipBitHat = (ipModHat + 1) / 2;
-    ipBitHat = reshape(ipBitHat.', nDSC * nSym, 1).';
-    
-    % Count errors
-    nErr(ii) = sum(ipBitHat ~= ipBit);
-end
-
-% BER calculation
-simBer = nErr / (nSym * nDSC);
-theoryBer = 0.5 * erfc(sqrt(10.^(EbN0dB/10)));
-
-% Plot
-figure;
-semilogy(EbN0dB, theoryBer, 'bs-', 'LineWidth', 2);
-hold on;
-semilogy(EbN0dB, simBer, 'mx-', 'LineWidth', 2);
-axis([0 10 1e-5 1]);
-grid on;
-legend('Theory', 'Simulation');
-xlabel('Eb/No (dB)');
-ylabel('Bit Error Rate');
-title('BER for BPSK-OFDM');
-
+   % Taking FFT, the term (nFFT/sqrt(nDSC)) is for normalizing the power of transmit symbol to 1  
+   xt = (nFFT/sqrt(nDSC))*ifft(fftshift(xF.')).';     % Appending cylic prefix 
+   xt = [xt(:,[49:64]) xt]; 
+  
+   % Concatenating multiple symbols to form a long vector 
+   xt = reshape(xt.',1,nSym*80); 
+   
+   % Gaussian noise of unit variance, 0 mean 
+   nt = 1/sqrt(2)*[randn(1,nSym*80) + j*randn(1,nSym*80)]; 
+  
+   % Adding noise, the term sqrt(80/64) is to account for the wasted energy due to 
+cyclic prefix 
+   yt = sqrt(80/64)*xt + 10^(-EsN0dB(ii)/20)*nt; 
+  
+   % Receiver 
+   yt = reshape(yt.',80,nSym).'; % formatting the received vector into symbols 
+   yt = yt(:,[17:80]); % removing cyclic prefix 
+  
+   % converting to frequency domain 
+   yF = (sqrt(nDSC)/nFFT)*fftshift(fft(yt.')).';  
+   yMod = yF(:,[6+[1:nBitPerSym/2] 7+[nBitPerSym/2+1:nBitPerSym] ]);  
+  
+   % BPSK demodulation 
+   % +ve value --> 1, -ve value --> -1 
+   ipModHat = 2*floor(real(yMod/2)) + 1; 
+   ipModHat(find(ipModHat>1)) = +1; 
+   ipModHat(find(ipModHat<-1)) = -1; 
+  
+   % converting modulated values into bits 
+   ipBitHat = (ipModHat+1)/2; 
+   ipBitHat = reshape(ipBitHat.',nBitPerSym*nSym,1).'; 
+  
+   % counting the errors 
+   nErr(ii) = size(find(ipBitHat - ipBit),2); 
+  
+end 
+  
+simBer = nErr/(nSym*nBitPerSym); 
+theoryBer = (1/2)*erfc(sqrt(10.^(EbN0dB/10))); 
+  
+close all; Fig 
+semilogy(EbN0dB,theoryBer,'bs-','LineWidth',2); 
+hold on 
+semilogy(EbN0dB,simBer,'mx-','LineWidth',2); 
+axis([0 10 10^-5 1]) 
+grid on 
+legend('theory', 'simulation'); 
+xlabel('Eb/No, dB') 
+ylabel('Bit Error Rate') 
+title('Bit error probability curve for BPSK using OFDM')
